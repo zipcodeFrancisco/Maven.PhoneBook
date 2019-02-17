67f893b70d937c05f199eab6322d4e2abb98f6e2
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-02-16 22:00:58 -0500

    Completed and Tested(100%)

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,100 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/23/18.
  */
-public class PhoneBook {
+public class PhoneBook { // Class signature
+
+    private String name; // Instance variable
+    private ArrayList<String> listNumbers; // Instance variable
+
+    //TreeMap<String, ArrayList<String>> directory; // Instance Object
+    TreeMap<String, ArrayList> directory; // Instance Object
+
+    public String removeFirstLastStrings( String str ){
+        return str.substring(1,str.length()-1);
+    }
+
+
+    public PhoneBook(){
+        this.directory = new TreeMap<String, ArrayList>();
+    }
+
+    // Adds an entry to the composite associate data type
+    public void add(String name, String number) { // Method signature (SETTER)
+        this.listNumbers = new ArrayList<String>();
+        if (directory.size() > 0 ){
+            if( !directory.containsKey(name) ){
+                listNumbers.add(number);
+                directory.put(name,listNumbers);
+            }
+            else{
+                if (!directory.get(name).contains(number)){
+                    listNumbers.add( removeFirstLastStrings( String.valueOf(directory.get(name)) )) ;
+                    listNumbers.add( number );
+                    directory.put(name,listNumbers);
+                }
+                else {
+
+                }
+            }
+        }
+        else {
+            listNumbers.add(number);
+            directory.put(name,listNumbers);
+        }
+    }
+
+    // Removes an entry to the composite associate data type
+    public void remove(String name){ // Method signature (SETTER)
+        if(directory.size() > 0){
+            directory.remove(name);
+        }
+    }
+
+    // Returns a phone number for the respective input name
+    public String lookup(String name){ // Method signature (GETTER)
+        if(directory.size() > 0){
+            return removeFirstLastStrings(directory.get(name).toString());
+        }
+        return null;
+    }
+
+    // Returns a name for the respective input phoneNumber
+    public String reverseLookup(String number){ // Method signature (GETTER)
+        if(directory.size() > 0){
+            for ( Map.Entry s: directory.entrySet() ) {
+                if( ((ArrayList<String>)s.getValue()).contains(number) ){
+                    return s.getKey().toString();
+                }
+            }
+        }
+      return null;
+    }
+
+    // Print a human-readable list of all entries (names and phone numbers) in alphabetical order.
+    public String display(){ // Method signature (GETTER)
+        String container = "";
+        for ( Map.Entry s : directory.entrySet() ) {
+            container += s.getKey() + " " + removeFirstLastStrings(s.getValue().toString()) + "\n";
+        }
+        return container.substring(0,container.length()-1);
+    }
+
+    public ArrayList<String> flattenCollection( Collection collection ){
+        ArrayList<String> container = new ArrayList<String>();
+        for (int i = 0; i < collection.size(); i++) {
+            ArrayList<String> xx = (ArrayList<String>)collection.toArray()[i];
+            for (int j = 0; j < xx.size(); j++) {
+                container.add( xx.get(j) );
+            }
+        }
+        return container;
+    }
+
+    public static void main(String[] args) {
+    }
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,92 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void addTest(){
+        // Given
+        String name = "Zebra";
+        String phoneNumber = "111-222-3333";
+        String expected = name;
+        PhoneBook phoneBook = new PhoneBook();
+        // When
+        phoneBook.add(name,phoneNumber);
+        String actual = phoneBook.reverseLookup(phoneNumber);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void removeTest(){
+        // Given
+        String name = "Zebra";
+        String phoneNumber = "111-222-3333";
+        String expected = null;
+        PhoneBook phoneBook = new PhoneBook();
+        // When
+        phoneBook.remove(name);
+        String actual1 = phoneBook.lookup(name);
+        String actual2 = phoneBook.lookup(phoneNumber);
+
+        //Then
+        Assert.assertEquals(expected,actual1);
+        Assert.assertEquals(expected,actual2);
+    }
+
+    @Test
+    public  void lookupTest(){
+        // Given
+        String name = "Zebra";
+        String phoneNumber = "111-222-3333";
+        String expected = phoneNumber;
+        PhoneBook phoneBook = new PhoneBook();
+        // When
+        phoneBook.add(name,phoneNumber);
+        String actual = phoneBook.lookup(name);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void reverseLookupTest(){
+        // Given
+        String name = "Zebra";
+        String phoneNumber = "111-222-3333";
+        String expected = name;
+
+        // When
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add(name,phoneNumber);
+
+        String actual = phoneBook.reverseLookup( phoneNumber );
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void diplayTest(){
+        // Given
+        String name1 = "Dog";
+        String phoneNumber1 = "222-444-4444";
+        String name2 = "Zebra";
+        String phoneNumber2 = "111-222-3333";
+        String expected = name1 + " " + phoneNumber1 + "\n" +
+                          name2 + " " + phoneNumber2;
+        // When
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add(name1, phoneNumber1);
+        phoneBook.add(name2, phoneNumber2);
+
+        String actual = phoneBook.display();
+
+        Assert.assertEquals(expected,actual);
+    }
 }
